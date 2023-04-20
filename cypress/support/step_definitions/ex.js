0f1fcdb0e7d383a8ee1cbd8106f8e04905737272
Sukhmani-Kaur1 <sukhmani.dev006@gmail.com>
2023-04-20 22:06:02 +0530

    Cypress configurations completed

---
+++ cypress/support/step_definitions/ex.js
@@ -0,0 +1,13 @@
+import { When, Then } from "cypress-cucumber-preprocessor/steps";
+
+When("I visit duckduckgo.com", () => {
+  cy.visit("https://www.duckduckgo.com");
+});
+
+Then("I should see a search bar", () => {
+  cy.get("input").should(
+    "have.attr",
+    "placeholder",
+    "Search the web without being tracked"
+  );
+});
